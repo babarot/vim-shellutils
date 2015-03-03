9dfa993e679380b0f06b7064104c1468fda72fca
b4b4r07 <b4b4r07@gmail.com>
2015-03-03 23:30:20 +0900

    Rename buttons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-var client = new ZeroClipboard( document.getElementById("copy-button") );
+var curl = new ZeroClipboard( document.getElementById("curl-button") );
 
-client.on( "ready", function( readyEvent ) {
+curl.on( "ready", function( readyEvent ) {
   // alert( "ZeroClipboard SWF is ready!" );
 
-  client.on( "aftercopy", function( event ) {
-    // `this` === `client`
+  curl.on( "aftercopy", function( event ) {
+    // `this` === `curl`
     // `event.target` === the element that was clicked
     event.target.style.display = "none";
     alert("Copied text to clipboard: " + event.data["text/plain"] );
   } );
 } );
+
+
+var wget = new ZeroClipboard( document.getElementById("wget-button") );
+
+wget.on( "ready", function( readyEvent ) {
+  // alert( "ZeroClipboard SWF is ready!" );
+
+  wget.on( "aftercopy", function( event ) {
+    // `this` === `wget`
+    // `event.target` === the element that was clicked
+    event.target.style.display = "none";
+    alert("Copied text to clipboard: " + event.data["text/plain"] );
+  } );
+} );
+
